46dc45ca7b26bc075414c1ab6151cc2be3d93793
bobbyxng <bobbyxng@gmail.com>
2026-08-20 12:00:57 +0200

    bug fix nvim init lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,3 +2,8 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+
+local ok, matugen = pcall(require, 'matugen')
+if ok then
+    pcall(matugen.setup)
+end
