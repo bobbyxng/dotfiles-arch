86969c47c1c640c5592f1daae62bf887692cf601
bobbyxng <bobbyxng@gmail.com>
2026-09-16 12:14:27 +0200

    nvim: add copy paste key-binds

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,3 +7,15 @@ local ok, matugen = pcall(require, 'matugen')
 if ok then
     pcall(matugen.setup)
 end
+
+-- Copy in visual mode with Ctrl+C
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to system clipboard" })
+
+-- Paste in insert mode with Ctrl+V
+vim.keymap.set("i", "<C-v>", '<C-r>+', { desc = "Paste from system clipboard" })
+
+-- Paste in normal mode with Ctrl+V
+vim.keymap.set("n", "<C-v>", '"+p', { desc = "Paste from system clipboard" })
+
+-- Paste in command-line mode (e.g. in : or / prompts)
+vim.keymap.set("c", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard" })
